d95f1276d3e7e177dc9fb5efb751f4f492df4a94
f-m-tamim15 <fmtamim15499@gmail.com>
2021-02-08 21:36:18 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,136 @@
+
+
+
+const displayItem = () => {
+
+const button =  document.getElementById('input').value
+  fetch (`https://www.themealdb.com/api/json/v1/1/search.php?s=${button}`)
+  .then(res => res.json())
+  .then(data => displayCountries(data.meals))
+  
+  const displayCountries = countries => {
+    const countriesDiv = document.getElementById('countries');
+    countriesDiv.innerHTML = "";
+   countries.forEach(country => {
+    // const country = countries[i];
+        // const countryDiv = document.createElement('div');
+    
+      
+    // countryDiv.className = 'country';
+    
+    countriesDiv.innerHTML +=  `
+<div class="country">
+      <img src ="${country.strMealThumb}"
+     <h3 class='meal-name'>${country.strMeal}</h3>
+     <p>${country.strArea} </p>
+      <button onclick="displayIngredients('${country.idMeal}')"> intgredients</button>
+  
+      </div>
+
+    `
+
+    // countryDiv.innerHTML = countryInfo ;
+    
+    
+    // countriesDiv.appendChild(countryDiv);
+     
+   });
+    
+  }
+  
+}
+console.log("helo");
+// const item = document.getElementById("target");
+
+
+const displayIngredients = id => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  // console.log(url);
+
+  fetch(url)
+  .then(res => res.json())
+  .then(data => chicken(data.meals[0]));
+  
+
+
+function chicken (item){
+   const lastDiv = document.createElement('div');
+const ingerdinetDiv = document.getElementById("initem") ;
+  const lastDivInfo = 
+  `
+
+  <h2> ${item.strMeal}</h2>
+    <ul>
+
+    <li>${item.strIngredient1}</li>
+    <li>${item.strIngredient2}</li>
+    <li>${item.strIngredient3}</li>
+    <li>${item.strIngredient4}</li>
+    <li>${item.strIngredient5}</li>
+    <li>${item.strIngredient6}</li>
+    <li>${item.strIngredient7}</li>
+    <li>${item.strIngredient8}</li>
+    <li>${item.strIngredient9}</li>
+
+
+
+    
+    </ul>
+  
+   
+ `
+  
+
+lastDiv.innerHTML = lastDivInfo;
+ ingerdinetDiv.appendChild(lastDiv);
+}
+
+}
+
+
+   
+// item.insertAdjacentHTML ("beforebegin" ,  `<li>${data.meals[0].strIngredient1},${data.meals[0].strIngredient2},${data.meals[0].strIngredient3} </li>`
+
+
+  // console.log(data.meals[0])
+
+  // console.log(data.meals[0].strIngredient1);
+
+//  const ingerdinet = item => {
+//    data.meals[0] = item;
+//    alert(.strIngredient1);
+//  }
+
+
+
+
+  //   inGredients(data.meals[0]) );
+
+
+  // const inGredientsItem = inGredients => {
+
+  //     alert(inGredients[0]);
+
+  // }console.log(data.meals[0].strIngredient1)
+  //console.log(data.meals[0])
+  // const ingerdinetDiv = document.createElement('div');
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i = 3; i < value.length; i++){
+//   if(value[i] === null || value[i] === " "){
+//       break;
+//   }
+//   else{
+//       console.log(value[i])
+//   }
+// }
